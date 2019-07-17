86b2653b947bc7b6c5324c933365891c6d14e23f
stackbaxter <stackbaxter@yahoo.com>
2019-07-17 03:04:24 -0500

    Abi was parsing weird from a JSON file. Successful Contract call function.

--- Source/nt.Shared/Constants/ContractConstants/NftCreator/Addresses.cs
+++ Source/nt.Shared/Constants/ContractConstants/NftCreator/Addresses.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace nt.Shared.Constants.ContractConstants.NftCreator
+﻿namespace nt.Shared.Constants.ContractConstants.NftCreator
 {
-    public class NftCreatorAddresses
+  public class NftCreatorAddresses
     {
         public static string NftCreatorRopstenAddress = "0x414bABeEc0a0Faa22D9FdbfaE461ace4c632a020";
         public static string NftCreatorRinkebyAddress = "0x10DF15C1BD76050E96f04C44cE11a5e5CA0e935e";
