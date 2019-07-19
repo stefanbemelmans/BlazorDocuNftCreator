876fbde7649484042059a2d2452acf4ecade1fee
stackbaxter <stackbaxter@yahoo.com>
2019-07-19 14:49:31 -0500

    directory namechange

---
+++ Source/nt.Client/Features/WebThree/WebThreeState.cs
@@ -0,0 +1,19 @@
+﻿namespace nt.Client.Features.WebThree
+{
+  using BlazorState;
+
+  internal partial class WebThreeState : State<WebThreeState>
+  {
+    public int TotalNftTypes { get; set; }
+
+    public int CurrentNftType { get; set; }
+
+    //public WebThreeState() { }
+
+    protected override void Initialize()
+    {
+      // Get TotalNftTypesAction
+      TotalNftTypes = 5;
+    }
+  }
+}
