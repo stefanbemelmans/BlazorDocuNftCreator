15732be1fafd1470e85dccb64b7d6f787df3b227
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-08-09 18:39:16 -0500

    refactor templates

--- Source/nt.Client/Features/WebThree/Components/NftTemplates/BillOfLading/BillOfLadingTemplate.razor.cs
+++ Source/nt.Client/Features/WebThree/Components/NftTemplates/BillOfLading/BillOfLadingTemplate.razor.cs
@@ -42,4 +42,19 @@
       }
     }
   }
+  public class BillOfLadingData : ImmutableObjectBase
+  {
+    public string Carrier { get; set; }
+    public string Consignee { get; set; }
+    public DateTime Date { get; set; }
+    public string Details { get; set; }
+    public string Instructions { get; set; }
+    public string ItemCode { get; set; }
+    public string Lading { get; set; }
+    public string Order { get; set; }
+    public string PO { get; set; }
+    public string Quantity { get; set; }
+    public string Shipper { get; set; }
+    public string Weight { get; set; }
+  }
 }
--- Source/nt.Client/Features/WebThree/Components/NftTemplates/RequestForProposal/RequestForProposalTemplate.razor.cs
+++ Source/nt.Client/Features/WebThree/Components/NftTemplates/RequestForProposal/RequestForProposalTemplate.razor.cs
@@ -39,4 +39,16 @@
       WebThreeState.MutableDataString = response.MutableDataString;
     }
   }
+  public class RequestForProposalData : ImmutableObjectBase
+  {
+    public string AcceptanceCriteria { get; set; }
+    public string AddressParcel { get; set; }
+    public string Budget { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactEmail { get; set; }
+    public string Deadline { get; set; }
+    public string EquivalentSubstitution { get; set; }
+    public string ModeOfTransport { get; set; }
+    public string Requirement { get; set; }
+  }
 }
