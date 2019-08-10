b7cb9af13a38a0b1a4656e894869bb1180b10441
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-08-10 07:58:05 +0000

    Form confirmation component

---
+++ Source/nt.Client/Features/WebThree/Components/FormConfirmationComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace nt.Client.Features.WebThree.Components
+{
+  using Microsoft.AspNetCore.Components;
+  using nt.Client.Features.WebThree.Components.NftTemplates;
+  using nt.Client.Features.Base.Components;
+
+  public class FormConfirmationComponentModel : BaseComponent
+  {
+    [Parameter]
+   public ImmutableObjectBase FormData { get; set; }
+
+    [Parameter]
+    public string MutableDataString { get; set; }
+
+
+  }
+}
